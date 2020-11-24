4c45372c8214b2b5ca5289d10453b8d32e67767c
Hélène Baudrand <baudrand.helene@gmail.com>
2020-11-24 11:35:07 +0100

    gitignore

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (isset($_POST['username'], $_POST['comment'])) {
     if ($message->isValid()) {
 
     } else {
-        $errors = 'Invalid form';
+        $errors = $message->getErrors();
     }
 }
 $title = 'Guest book';
@@ -16,15 +16,24 @@ require 'elements/header.php';
     <h1>Guest Book</h1>
     <?php if ($errors): ?>
         <div class="alert alert-danger">
-            <?= $errors ?>
+            Invalid form
         </div>
     <?php endif; ?>
     <form action="" method="post">
         <div class="form-group">
-            <input type="text" name="username" placeholder="enter your login" class="form-control">
+            <input value="<?= htmlentities($_POST['username'] ?? '') ?>" type="text" name="username"
+                   placeholder="enter your login"
+                   class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?>">
+            <?php if (isset($errors['username'])): ?>
+                <div class="invalid-feedback"><?= $errors['username'] ?></div>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <textarea name="comment" placeholder="enter your comment" class="form-control"></textarea>
+            <textarea name="comment" placeholder="enter your comment"
+                      class="form-control <?= isset($errors['comment']) ? 'is-invalid' : '' ?>"><?= htmlentities($_POST['comment'] ?? '') ?></textarea>
+            <?php if (isset($errors['comment'])): ?>
+                <div class="invalid-feedback"><?= $errors['comment'] ?></div>
+            <?php endif; ?>
         </div>
         <button class="btn btn-primary">Submit</button>
     </form>
