f66dca2f70f24dc86a44e245dedb9bfe8f8b60a6
Hélène Baudrand <baudrand.helene@gmail.com>
2020-11-26 17:50:02 +0100

    installed PDO

---
+++ blog/index.php
@@ -0,0 +1,28 @@
+<?php
+$pdo = new PDO('sqlite:../data.db', null, null, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+]);
+$error = null;
+try {
+    $query = $pdo->query('SELECT * FROM posts');
+    $posts = $query->fetchAll();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+}
+require '../elements/header.php';
+?>
+
+<?php if ($error): ?>
+    <div class="alert alert-danger"><?= $error ?></div>
+<?php else: ?>
+    <ul>
+        <?php foreach ($posts as $post): ?>
+            <li><?= $post->name ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<?php
+require '../elements/footer.php';
+?>
